0e628744002a70515859bff43baa2a38807bfab7
William Rockwood <wrockwood@tadl.org>
2025-06-23 13:52:50 -0400

    fix omniauth
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,13 +8,20 @@ OmniAuth.config.allowed_request_methods = %i[get post]
 OmniAuth.config.silence_get_warning    = true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-    ENV.fetch('GOOGLE_CLIENT_ID'),
-    ENV.fetch('GOOGLE_CLIENT_SECRET'),
-    {
-      scope:  'userinfo.email,userinfo.profile',
-      hd:     'tadl.org',
-      prompt: 'select_account'
-    }
+  google_id     = ENV['GOOGLE_CLIENT_ID']
+  google_secret = ENV['GOOGLE_CLIENT_SECRET']
+
+  if google_id.present? && google_secret.present?
+    provider :google_oauth2,
+      google_id,
+      google_secret,
+      {
+        scope:  'userinfo.email,userinfo.profile',
+        hd:     'tadl.org',
+        prompt: 'select_account'
+      }
+  else
+    Rails.logger.warn "[OmniAuth] Skipping Google provider: GOOGLE_CLIENT_ID/SECRET not set"
+  end
 end
 
