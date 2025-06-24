8fb002a9915c36f83fb89d6f14e962d89d93afee
William Rockwood <wrockwood@tadl.org>
2025-06-24 17:51:37 -0400

    lmao
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/initializers/rails_admin_turbo_stream_fallback.rb
+++ config/initializers/rails_admin_turbo_stream_fallback.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
 # config/initializers/rails_admin_turbo_stream_fallback.rb
 
-# Monkey-patch RailsAdmin’s controller to coerce turbo_stream into HTML
-RailsAdmin::ApplicationController.class_eval do
-  before_action :force_html_for_turbo_stream
+# Monkey-patch RailsAdmin::MainController so that if RailsAdmin
+# ever raises UnknownFormat (e.g. a Turbo Stream request),
+# we force the request back to HTML and retry.
+Rails.application.config.to_prepare do
+  # Make sure the class is loaded before we patch
+  require 'rails_admin/main_controller'
 
-  private
-
-  def force_html_for_turbo_stream
-    if request.format.symbol == :turbo_stream
+  RailsAdmin::MainController.class_eval do
+    rescue_from ActionController::UnknownFormat do
       request.format = :html
+      retry
     end
   end
 end
