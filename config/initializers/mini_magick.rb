b92e39a6c20a0959d5546335ca6be2279fab3c8a
William Rockwood <wrockwood@tadl.org>
2025-06-24 17:38:07 -0400

    fix mini_magick initializer
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/initializers/mini_magick.rb
+++ config/initializers/mini_magick.rb
@@ -2,9 +2,10 @@
 require "mini_magick"
 
 MiniMagick.configure do |config|
-  # use the ImageMagick CLI
-  config.cli = :imagemagick
-
-  # if you’re on dokku with the apt buildpack, ImageMagick will live under .apt
+  # make sure we use ImageMagick’s `convert`/`identify`
+  config.cli     = :imagemagick
+  # point at the system‐installed binary
   config.cli_path = "/usr/bin"
+  # don’t blow up if you validate on create; you’ll still see errors at processing time
+  config.validate_on_create = true
 end
