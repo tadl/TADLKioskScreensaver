26acea2713704330a8f53ca65c6ac4c10f23ff74
William Rockwood <wrockwood@tadl.org>
2025-06-24 13:34:09 -0400

    and again
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,21 +1,24 @@
 # config/initializers/rails_admin.rb
 
 RailsAdmin.config do |config|
-  config.asset_source        = :importmap
-  config.parent_controller   = '::ApplicationController'
+  config.asset_source      = :importmap
+  config.parent_controller = '::ApplicationController'
 
   # == Authentication ==
   config.authenticate_with do
     redirect_to main_app.sign_in_path unless user_signed_in?
   end
+
+  # current_user comes from your ApplicationController#current_user
   config.current_user_method(&:current_user)
 
   # == Authorization ==
   config.authorize_with :cancancan
 
   # == UI ==
-  config.main_app_name            = ['Kiosk Screensaver', 'Admin']
-  config.included_models         = %w[KioskGroup Kiosk Slide Permission UserPermission]
+  config.main_app_name   = ['Kiosk Screensaver', 'Admin']
+  config.included_models = %w[KioskGroup Kiosk Slide Permission UserPermission]
+
   config.navigation_static_label = 'Account'
   config.navigation_static_links = {
     'Sign out' => '/sign_out'
@@ -35,12 +38,17 @@ RailsAdmin.config do |config|
 
   # == Permission ==
   config.model 'Permission' do
-    visible { bindings[:controller].current_ability.can?(:manage, Permission) }
+    visible do
+      bindings[:controller].current_ability.can?(:manage, Permission)
+    end
   end
 
   # == UserPermission ==
   config.model 'UserPermission' do
-    visible { bindings[:controller].current_ability.can?(:manage, UserPermission) }
+    visible do
+      bindings[:controller].current_ability.can?(:manage, UserPermission)
+    end
+
     list do
       field :user
       field :permission
@@ -60,24 +68,18 @@ RailsAdmin.config do |config|
     end
 
     edit do
-      field :name do
-        read_only { !bindings[:controller].current_ability.can?(:manage, KioskGroup) }
-      end
-      field :slug do
-        read_only { !bindings[:controller].current_ability.can?(:manage, KioskGroup) }
-      end
-      field :kiosks do
-        read_only { !bindings[:controller].current_ability.can?(:manage, KioskGroup) }
-      end
+      field(:name)  { read_only { !bindings[:controller].current_ability.can?(:manage, KioskGroup) } }
+      field(:slug)  { read_only { !bindings[:controller].current_ability.can?(:manage, KioskGroup) } }
+      field(:kiosks){ read_only { !bindings[:controller].current_ability.can?(:manage, KioskGroup) } }
     end
   end
 
   # == Kiosk ==
   config.model 'Kiosk' do
-    navigation_label 'Content'
-    weight          1
-    label_plural    'Kiosks'
-    object_label_method :slug
+    navigation_label       'Content'
+    weight                 1
+    label_plural           'Kiosks'
+    object_label_method    :slug
 
     list do
       field :name
@@ -87,45 +89,44 @@ RailsAdmin.config do |config|
     end
 
     edit do
-      field :slides do
-        read_only { !bindings[:controller].current_ability.can?(:manage, Slide) }
-      end
-      field :name do
-        read_only { !bindings[:controller].current_ability.can?(:manage, Kiosk) }
-      end
-      field :slug do
-        read_only { !bindings[:controller].current_ability.can?(:manage, Kiosk) }
-      end
-      field :catalog_url do
-        read_only { !bindings[:controller].current_ability.can?(:manage, Kiosk) }
-      end
-      field :kiosk_group do
-        read_only { !bindings[:controller].current_ability.can?(:manage, KioskGroup) }
-      end
+      field(:slides){ read_only { !bindings[:controller].current_ability.can?(:manage, Slide) } }
+      field(:name)  { read_only { !bindings[:controller].current_ability.can?(:manage, Kiosk) } }
+      field(:slug)  { read_only { !bindings[:controller].current_ability.can?(:manage, Kiosk) } }
+      field(:catalog_url){ read_only { !bindings[:controller].current_ability.can?(:manage, Kiosk) } }
+      field(:kiosk_group){ read_only { !bindings[:controller].current_ability.can?(:manage, KioskGroup) } }
     end
   end
 
   # == Slide ==
   config.model 'Slide' do
-    navigation_label 'Content'
-    weight          2
-    label_plural    'Slides'
+    navigation_label    'Content'
+    weight              2
+    label_plural        'Slides'
     object_label_method :rails_admin_label
 
     list do
       field :image do
         label    'Preview'
         sortable false
+
         formatted_value do
+          # guard against dashboard or any non-view context
+          unless defined?(bindings) && bindings[:object] && bindings[:view]
+            next '-'
+          end
+
           slide = bindings[:object]
           if slide.image.attached?
-            thumb = slide.image.variant(resize_to_limit: [100, 100]).processed
-            url   = Rails.application.routes.url_helpers.
-                      rails_representation_url(thumb, host: bindings[:view].request.base_url)
+            variant = slide.image.variant(resize_to_limit: [100, 100]).processed
+            url     = Rails.application.routes.url_helpers.
+                        rails_representation_url(variant,
+                                                 host: bindings[:view].request.base_url)
             bindings[:view].tag.img(src: url, width: 100, height: 100)
           else
             '-'
           end
+        rescue StandardError
+          '-'
         end
       end
 
@@ -133,7 +134,6 @@ RailsAdmin.config do |config|
       field :display_seconds
       field :start_date
       field :end_date
-
       field :kiosks do
         label    'Assigned Kiosks'
         sortable false
@@ -144,18 +144,14 @@ RailsAdmin.config do |config|
       field :title
 
       field :image, :active_storage do
-        # only persisteds get a signed_id
         cache_value do
-          rec = bindings[:object]
-          rec.persisted? && rec.image.attached? ? rec.image.signed_id : nil
-        rescue
-          nil
-        end
-
-        resource_url do
-          rec = bindings[:object]
-          rec.image.attached? ? bindings[:view].url_for(rec.image) : nil
-        rescue
+          # only attempt to read a cache when we actually have a persisted record
+          record = (defined?(bindings) && bindings[:object])
+          if record&.persisted?
+            attach = record.send(name)
+            attach.signed_id if attach.attached?
+          end
+        rescue StandardError
           nil
         end
       end
@@ -163,7 +159,6 @@ RailsAdmin.config do |config|
       field :display_seconds
       field :start_date
       field :end_date
-
       field :kiosks do
         read_only { !bindings[:controller].current_ability.can?(:manage, Slide) }
       end
