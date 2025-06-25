5f4d50cdc098d7fa131e4055cdc31958ef19aaeb
William Rockwood <wrockwood@tadl.org>
2025-06-25 10:50:57 -0400

    Revert "end end end end end end"
    This reverts commit 9417cd153630c8ffef0098fd664fafb870e808a2.

--- config/initializers/rails_admin_turbo_stream_fallback.rb
+++ config/initializers/rails_admin_turbo_stream_fallback.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 # config/initializers/rails_admin_turbo_stream_fallback.rb
 
-# If RailsAdmin ever receives a turbo_stream request it can’t handle,
+# If RailsAdmin ever gets a turbo_stream request it can’t handle,
 # convert it to HTML so the normal RailsAdmin views will render.
 Rails.application.config.to_prepare do
-  # only if RailsAdmin is loaded
-  if defined?(RailsAdmin::MainController)
-    RailsAdmin::MainController.class_eval do
-      # before every action, downgrade turbo_stream to html
-      prepend_before_action do
-        if request.format.symbol == :turbo_stream
-          request.format = :html
-        end
+  # make sure the controller is loaded
+  require 'rails_admin/main_controller'
+
+  RailsAdmin::MainController.class_eval do
+    # run before every action
+    prepend_before_action do
+      # in Rails 7 Turbo, request.format.symbol == :turbo_stream
+      if request.format.symbol == :turbo_stream
+        request.format = :html
       end
     end
   end
