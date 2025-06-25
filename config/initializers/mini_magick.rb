52e915c0406e231935c94c2a095dd7d197f17e34
William Rockwood <wrockwood@tadl.org>
2025-06-25 10:50:57 -0400

    Revert "fix mini_magick initializer"
    This reverts commit b92e39a6c20a0959d5546335ca6be2279fab3c8a.

--- config/initializers/mini_magick.rb
+++ config/initializers/mini_magick.rb
@@ -2,10 +2,9 @@
 require "mini_magick"
 
 MiniMagick.configure do |config|
-  # make sure we use ImageMagick’s `convert`/`identify`
-  config.cli     = :imagemagick
-  # point at the system‐installed binary
+  # use the ImageMagick CLI
+  config.cli = :imagemagick
+
+  # if you’re on dokku with the apt buildpack, ImageMagick will live under .apt
   config.cli_path = "/usr/bin"
-  # don’t blow up if you validate on create; you’ll still see errors at processing time
-  config.validate_on_create = true
 end
