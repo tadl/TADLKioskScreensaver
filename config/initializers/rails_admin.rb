a4caeef57f1887a71e20d4cc3e898c252ce872ce
William Rockwood <wrockwood@tadl.org>
2025-06-25 10:50:57 -0400

    Revert "another"
    This reverts commit 749f4eb78a240bef0374ae193edcb0ad8dbb3207.

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -2,19 +2,18 @@
 
 Rails.application.config.to_prepare do
   #
-  # 1) Disable Turbo/AJAX on all RailsAdmin forms
+  # 1) Force all RailsAdmin forms to submit without Turbo/AJAX
   #
-  begin
-    RailsAdmin::MainHelper.module_eval do
-      def rails_admin_form_for(object, **options, &block)
-        options[:local] ||= true
-        options[:html]    ||= {}
-        options[:html]['data-turbo'] = false
-        super(object, **options, &block)
-      end
+  require 'rails_admin/main_helper'
+  RailsAdmin::MainHelper.module_eval do
+    def rails_admin_form_for(object, **options, &block)
+      # make it a plain HTML form
+      options[:local] = true
+      options[:html] ||= {}
+      options[:html]['data-turbo'] = false
+
+      super(object, **options, &block)
     end
-  rescue NameError
-    # RailsAdmin::MainHelper not loaded yet—will retry on next to_prepare
   end
 
   #
@@ -57,7 +56,7 @@ Rails.application.config.to_prepare do
 end
 
 #
-# 4) The regular RailsAdmin config
+# 4) Your normal RailsAdmin.config block
 #
 RailsAdmin.config do |config|
   config.asset_source      = :importmap
