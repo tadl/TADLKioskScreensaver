46ef930181ec90be8c3647df58e8495a2971f265
William Rockwood <wrockwood@tadl.org>
2025-06-30 12:57:35 -0400

    fix up routes
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,21 @@ Rails.application.routes.draw do
   match  '/sign_out', to: 'sessions#destroy', as: :sign_out, via: [:get, :delete]
   get '/auth/:provider/callback', to: 'sessions#create'
 
-  %w(404 422 500).each do |code|
+  # ---- Custom error pages ----
+  %w(403 404 422 500).each do |code|
+    # handle both explicit hits ("/404") and internal exception dispatch
     match code, to: "errors#show", via: :all, defaults: { code: code }
   end
+
+  # catch any _other_ unmatched route and treat it as a 404
+  match "*unmatched", to: "errors#show", via: :all, defaults: { code: "404" }
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: "screensaver#index"
 
-  resources :slides, except: [:show]
-  resources :kiosks, except: [:show]
-  resources :kiosk_groups, except: [:show]
+#  resources :slides, except: [:show]
+#  resources :kiosks, except: [:show]
+#  resources :kiosk_groups, except: [:show]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
