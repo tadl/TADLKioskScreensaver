30b04b0975c09caa171ead9a4f0b864f64ca8ce7
William Rockwood <wrockwood@tadl.org>
2025-07-28 15:34:13 -0400

    improve report interface and reduce database queries per page load
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/models/kiosk_session.rb
+++ app/models/kiosk_session.rb
@@ -1,9 +1,9 @@
 # app/models/kiosk_session.rb
 class KioskSession < ApplicationRecord
-  def kiosk
-    Kiosk.find_by(slug: kiosk_code)
-  end
+  # Associates kiosk_sessions.kiosk_code => kiosks.slug
+  belongs_to :kiosk, primary_key: :slug, foreign_key: :kiosk_code, optional: true
 
+  # Convenience methods
   def kiosk_group
     kiosk&.kiosk_group
   end
