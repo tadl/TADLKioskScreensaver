bc07a39c37799940d74ee04cef091f8996ca099d
William Rockwood <wrockwood@tadl.org>
2025-08-05 13:38:05 -0400

    sync admin column of user model with admin user_permission
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/models/user_permission.rb
+++ app/models/user_permission.rb
@@ -7,7 +7,17 @@ class UserPermission < ApplicationRecord
 
   validates :permission_id, uniqueness: { scope: :user_id }
 
+  after_save :sync_admin_flag_on_user
+  after_destroy :sync_admin_flag_on_user
+
   def rails_admin_label
     user.email.split('@').first
+  end
+
+  private
+
+  def sync_admin_flag_on_user
+    is_admin = user.permissions.where(name: 'admin').exists?
+    user.update_column(:admin, is_admin)
   end
 end
